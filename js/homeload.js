940a623ff9543ddf36a5a1e4eca8a81709311a4b
velora <serenityios41@gmail.com>
2024-12-01 17:33:45 +0100

    bang bang bang

--- js/homeload.js
+++ js/homeload.js
@@ -71,7 +71,7 @@ const loadApps = async (searchTerm = '') => {
 
 const openModal = (app) => {
     const modalHtml = `
-        <div id="modal" style="left: 0; display: block; width: 100vw; overflow-x: hidden; height: calc(100vh + 48px); background-color: #131416; position: fixed; top; -48px; z-index: 1000000000000; opacity: 0; visibility: hidden; transition: opacity 0.3s ease;">
+        <div id="modal" style="left: 0; display: block; width: 100vw; overflow-x: hidden; height: calc(100vh + 48px); background-color: #131416; position: fixed; top: env(safe-area-inset-top); z-index: 1000000000000; opacity: 0; visibility: hidden; transition: opacity 0.3s ease;">
             <div style="position: relative; top: 0; left: 0; width: 100%; height: 100%; overflow-x: hidden;">
                 <div class="image-wrapper" style="width: 100%; height: 200px; background-image: url('${app.iconURL}'); background-size: 3500% 3500%; background-position: bottom right; background-repeat: no-repeat;">
                 </div>
